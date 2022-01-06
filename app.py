5cdd1724809aae08ae2ca2c01aea1282eb30c964
Farhan Molla <farhanmolla@Farhans-MacBook-Air.local>
2022-01-05 20:34:39 -0500

    finished the framework

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
-
+from flask import render_template
+from flask import request
+from flask import url_for
+from flask import redirect
 
 # -- Initialization section --
 app = Flask(__name__)
@@ -13,4 +14,4 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    return render_template('index.html')
