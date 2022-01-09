d2c6b9b4ec8157de926b4bbfaa574cb0b3c744ad
melkadze <25963683+melkadze@users.noreply.github.com>
2022-01-09 17:52:28 -0500

    demo endpoint with url arguments

--- app.py
+++ app.py
@@ -14,4 +14,15 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
+
+# The API route is what the client will contact to get its matches
+@app.route('/api')
+def api():
+	# Request arguments, like id, are used by accessing:
+	# www.example.com/api?id=X&num=Y
+	# There is currently no handling for URLs which do not
+	# provide all of the arguments we are using
+	id = request.args['id']
+	num = request.args['num']
+	return "API Ok for id " + id + " and num " + num
