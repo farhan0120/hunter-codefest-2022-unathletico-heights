f20797f715e85b7b0b143a75074ed3c3ad6fc1d1
Adrian-Mok15 <ADrian.Mok15@myhunter.cuny.edu>
2022-01-23 22:54:57 -0500

    Restored files

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from flask import render_template
 from flask import request
 from flask import url_for
 from flask import redirect
+import csv # for /api endpoint
+
 
 # -- Initialization section --
 app = Flask(__name__)
@@ -19,14 +21,88 @@ def index():
 # The API route is what the client will contact to get its matches
 @app.route('/api')
 def api():
+	## Set up arguments
 	# Request arguments, like players, are used by accessing:
-	# www.example.com/api?players=X&time=X&age=X&difficulty=X&popularity=X
+	# www.example.com/api?playersmin=X&playersmax=X&time=X&age=X&difficulty=X&popularity=X&result=X
 	# Note: there is currently no handling for URLs which do not
 	# provide all of the arguments we are using
-	players = request.args['players']
-	time = request.args['time'] # in minutes
-	age = request.args['age'] # player age in years
-	difficulty = request.args['difficulty'] # from 1-5
-	popularity = request.args['popularity'] # from 1-100,
-			# 1 being most; corresponds to rank
-	return "API OK for [players, time, age, difficulty, popularity] as [" + players + ", " + time + ", " + age + ", " + difficulty + ", " + popularity + "]"
+	# Definition : 	a strong argument is one that MUST be met in all results
+	# 		a weak argument is one that we want to be as close as we can to
+	playersmin = int(request.args['playersmin'])
+	playersmax = int(request.args['playersmax'])
+	time = int(request.args['time']) # in minutes
+	age = int(request.args['age']) # player age in years
+	difficulty = float(request.args['difficulty']) # from 0-1
+	popularity = float(request.args['popularity']) # from 0-1,
+			# 0 being most; corresponds to rank
+	result = int(request.args['result']) - 1 # which result, starts at 1;
+			# present user with result=1, then if they decline, result=2, etc;
+			# we subtract it from 1 here to compensate for 0-indexing
+
+	## Set up CSV
+	header = []
+	rows = []
+	with open('database.csv', newline='') as csvfile:
+		csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+		header = next(csvreader)
+		for row in csvreader:
+			rows.append(row)
+
+	## Start eliminating
+	# By minutes (strong)
+	toRemove = []
+	for game in rows:
+		minutesarray = game[2].split() # split "20 40 Min"
+		if "Time:" not in minutesarray: # this means there is no time
+			minutesarray.remove("Min")
+			if len(minutesarray) == 1 and time != int(minutesarray[0]):
+				toRemove.append(game)
+			elif len(minutesarray) == 2 and (time < int(minutesarray[0]) or time > int(minutesarray[1])):
+				toRemove.append(game)
+		else:
+			toRemove.append(game) # remove even if no time given, as we can't know if it matches user's request	
+	for game in toRemove:
+		rows.remove(game)
+	
+	# By players (strong)
+	toRemove = []
+	for game in rows:
+		playersarray = game[1].split() # split "2 4 Players"
+		playersarray.remove("Players")
+		if len(playersarray) == 1 and (playersmin != int(playersarray[0]) or playersmax != int(playersarray[0])):
+			toRemove.append(game)
+		elif len(playersarray) == 2 and (playersmax < int(playersarray[0]) or playersmin > int(playersarray[1])):
+			toRemove.append(game)
+	for game in toRemove:
+		rows.remove(game)
+
+	# By age (strong)
+	toRemove = []
+	for game in rows:
+		agearray = game[3].split("Age: ")[1][0:-1] # extract number
+		if (agearray != ' ' and int(agearray) > age): # if age is nonempty and greater than given value
+			toRemove.append(game)
+	for game in toRemove:
+		rows.remove(game)
+
+	# By difficulty and popularity (weak)
+	distancearray = []
+	for game in rows:
+		difficultyarray = game[4].split('/')
+		gamediff = float(difficultyarray[0].replace(',', '')) / float(difficultyarray[1].replace(',', ''))
+		popularityarray = game[5].split('/')
+		gamepop = float(popularityarray[0].replace(',', '')) / float(popularityarray[1].replace(',', ''))
+		distancearray.append(pow((difficulty - gamediff) + (popularity - gamepop), 2))
+	
+	# Sort weak results
+	resultarray = [x for y, x in sorted(zip(distancearray, rows))]
+		# zips together the two arrays (combines them)
+		# sorts them (by the combined distance array)
+		# and removes the distance values after sorting
+
+	# Return result
+	if len(resultarray) > result: # if result exists
+		return render_template("api.html", status="OK", name=resultarray[result][0], players=resultarray[result][1], time=resultarray[result][2], age=resultarray[result][3], difficulty=resultarray[result][4], rank=resultarray[result][5], description=resultarray[result][6])
+		output = ">>>>>".join(resultarray[result])
+	else:
+		return render_template("api.html", status="ERROR")
